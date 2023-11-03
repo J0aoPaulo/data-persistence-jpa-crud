c42e46864bf80c0fc06df812fa1d6c5a76f8d27e
Joao-Paulo <ojoaopaulo1324@gmail.com>
2023-11-03 17:55:59 -0300

    [I#	new file:   mvnw

---
+++ src/test/java/com/persistJPHM/sistemapersistencia/SistemaPersistenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.persistJPHM.sistemapersistencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPersistenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
