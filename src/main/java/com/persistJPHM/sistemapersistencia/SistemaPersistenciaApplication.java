750ba28509c01c43c934484be032cf1eedf673b7
João Paulo <joao.almeida@alu.ufc.br>
2023-11-06 14:58:09 -0300

    teste-intellij

--- src/main/java/com/persistJPHM/sistemapersistencia/SistemaPersistenciaApplication.java
+++ src/main/java/com/persistJPHM/sistemapersistencia/SistemaPersistenciaApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SistemaPersistenciaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaPersistenciaApplication.class, args);
+
+
 	}
 
 }
