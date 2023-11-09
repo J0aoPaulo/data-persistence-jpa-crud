37e3b192dc8fd15ef7b7c0d5d18bfaff9d5c789c
João Paulo <joao.almeida@alu.ufc.br>
2023-11-09 13:14:37 -0300

    fix and comment usuario queries

--- src/main/java/com/persistJPHM/sistemapersistencia/DAO/UsuarioDAO.java
+++ src/main/java/com/persistJPHM/sistemapersistencia/DAO/UsuarioDAO.java
@@ -11,22 +11,29 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioDAO extends JpaRepository<Usuario, Integer> {
+    //Buscar Usuario por cpf
     Usuario findFirstByCpf(String cpf);
 
-    Usuario findByNameOrderByName(String cpf);
+    //Buscar por nome em ordem alfabética
+    Usuario findByNameOrderByName(String nome);
 
+    //Consultar nomes de Usuarios por um caractere específico
     @Query(value = "Select nome FROM Usuario WHERE LOWER(nome) LIKE :prefix%", nativeQuery = true)
     List<String> consultaPorLetra(@Param("prefix") String prefix);
 
+    //Procurar cpf pelos 3 primeiros numeros fornecidos
     @Query("SELECT u.cpf FROM Usuario WHERE SUBSTRING(u.cpf, 1, 3) = :tresPrimeirosNumeros")
     Set<String> procureCpfsComTresPrimeirosNumeros(@Param("tresPrimeirosNumeros") String tresPrimeirosNumeros);
 
+    //Procurar usuarios entre dois ‘ids’ diferentes
     @Query(name = "usuarioEntreID")
     public Usuario consultarUsuarioEntreId(String cpfInicial, String cpfFinal);
 
+    //Consultar Usuario por nome específico
     @Query("consultaPorNome")
     public Usuario consultaPorNomeEspecifico(String nome);
 
+    //Conta quantos usuarios existem
     @Query(value = "select count(*) from usuario u", nativeQuery = true)
     public long contaUsuarios();
 }
