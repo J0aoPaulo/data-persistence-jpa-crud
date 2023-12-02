91cb6044f65fc08228a583248c82e2c3e57c503f
hermesonbastos <hermesonbastos@gmail.com>
2023-12-02 17:49:25 -0300

    mais recente

--- src/main/java/com/persistJPHM/sistemapersistencia/repository/DescontoMongoDao.java
+++ src/main/java/com/persistJPHM/sistemapersistencia/repository/DescontoMongoDao.java
@@ -1,9 +1,7 @@
 package com.persistJPHM.sistemapersistencia.repository;
 
-import com.persistJPHM.sistemapersistencia.DAO.DescontoRecorrenteDAO;
 import com.persistJPHM.sistemapersistencia.entity.DescontoRecorrente;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface DescontoMongoDao extends DescontoRecorrenteDAO,
-        MongoRepository<DescontoRecorrente, String> {
+public interface DescontoMongoDao extends MongoRepository<DescontoRecorrente, String> {
 }
